b767b7ae3dbcfef98a07b506b476050fe8f99a27
Nave <naveguy@duck.com>
2025-03-13 15:22:56 -0700

    Update app.js

--- active/app.js
+++ active/app.js
@@ -1,4 +1,4 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// Do not edit unless you know what your doing
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });

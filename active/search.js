4cf4405d4d4f6967cda72d93ce239cb13e05c456
Nave <naveguy@duck.com>
2025-03-14 17:14:45 -0700

    Create search.js

---
+++ active/search.js
@@ -0,0 +1,18 @@
+const searchEngines = Object.freeze({
+    Google: 'google.com/search?q=',
+    Bing: 'bing.com/search?q=',
+    DuckDuckGo: 'duckduckgo.com/?q=',
+    Startpage: 'startpage.com/sp/search?query=',
+});
+
+const defaultSearch = searchEngines['DuckDuckGo'];
+
+const search = (input, template = `https://${defaultSearch}%s`) => {
+    try {
+        return new URL(input) + '';
+    } catch (e) {
+        const url = new URL(`http://${input}`);
+        if (url.hostname.indexOf('.') != -1) return url + '';
+        return template.replace('%s', encodeURIComponent(input));
+    }
+};
